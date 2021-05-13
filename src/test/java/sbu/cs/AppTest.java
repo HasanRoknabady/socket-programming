aa5f16d71302b63821007864186435c321a1cacd
Hasan Roknabady <hasanrokn@yahoo.com>
2021-05-13 13:41:09 +0430

    bugfix/test1

--- src/test/java/sbu/cs/AppTest.java
+++ src/test/java/sbu/cs/AppTest.java
@@ -1,9 +1,8 @@
 package sbu.cs;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertTrue;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
+import org.junit.Test;
 
 public class AppTest {
 
